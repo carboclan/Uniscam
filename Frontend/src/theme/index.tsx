e939974f5e4b96cb65c72bc9bf589861e0f2cb2d
Frank Wei <frank@frankwei.xyz>
2020-08-28 19:30:16 +0800

    style: change theme color

--- Frontend/src/theme/index.tsx
+++ Frontend/src/theme/index.tsx
@@ -59,7 +59,7 @@ export function colors(darkMode: boolean): Colors {
     advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.6)',
 
     //primary colors
-    primary1: darkMode ? '#2172E5' : '#ff007a',
+    primary1: darkMode ? '#2172E5' : '#0091FF',
     primary2: darkMode ? '#3680E7' : '#FF8CC3',
     primary3: darkMode ? '#4D8FEA' : '#FF99C9',
     primary4: darkMode ? '#376bad70' : '#F6DDE8',
