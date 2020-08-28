bf7556412396e37b2449b5c8f4de8b9da07a18f6
Frank Wei <frank@frankwei.xyz>
2020-08-28 17:44:24 +0800

    refactor: hide v1

--- Frontend/src/components/Header/index.tsx
+++ Frontend/src/components/Header/index.tsx
@@ -19,7 +19,6 @@ import Menu from '../Menu'
 
 import Row, { RowBetween } from '../Row'
 import Web3Status from '../Web3Status'
-import VersionSwitch from './VersionSwitch'
 
 const HeaderFrame = styled.div`
   display: flex;
@@ -168,7 +167,6 @@ export default function Header() {
             </AccountElement>
           </HeaderElement>
           <HeaderElementWrap>
-            <VersionSwitch />
             <Settings />
             <Menu />
           </HeaderElementWrap>
