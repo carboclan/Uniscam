2c13d57910f11b23ecb091224aa0bb03ee820ef7
lychees <lychees67@gmail.com>
2020-08-29 18:47:16 -0700

    梭哈。

--- Manager/contracts/yyCrv.sol
+++ Manager/contracts/yyCrv.sol
@@ -306,8 +306,8 @@ contract yyCrv is ERC20, ERC20Detailed, ReentrancyGuard, Ownable {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
-
     
+/*
     // rinkeby
     IERC20 constant public yCrv = IERC20(0x979981F8C17C19BaA66c8806579626269ef948d0);
     IERC20 constant public y3d = IERC20(0x7a672B200f906D56E8B528413d02D12abABcc231);
@@ -316,7 +316,8 @@ contract yyCrv is ERC20, ERC20Detailed, ReentrancyGuard, Ownable {
     address constant public crv_minter = address(0xd061D61a4d941c39E5453435B6345Dc261C2fcE0);
     address constant public uniswap = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
     address constant public crv_voting = address(0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2);
-/* 
+*/
+
     // mainnet
     IERC20 constant public yCrv = IERC20(0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8);
     IERC20 constant public y3d = IERC20(0xc7fD9aE2cf8542D71186877e21107E1F3A0b55ef);
@@ -326,7 +327,7 @@ contract yyCrv is ERC20, ERC20Detailed, ReentrancyGuard, Ownable {
     address constant public crv_minter = address(0xd061D61a4d941c39E5453435B6345Dc261C2fcE0);
     address constant public uniswap = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
     address constant public crv_voting = address(0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2);
-*/
+
     uint public pool;
     bool public beta = true;
 
