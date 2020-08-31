29a532f43cb21d2e414e2845405e6d2511bb7452
Frank Wei <frank@frankwei.xyz>
2020-08-31 21:40:20 +0800

    feat: new Router02 by zxplus

--- Frontend/src/constants/index.ts
+++ Frontend/src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x4D33eCC2eBE4EA5Df99Ea4878De164416bD13295'
 
 // a list of tokens by chain
 type ChainTokenList = {
