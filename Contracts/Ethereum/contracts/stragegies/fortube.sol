f9461c152aec4ab7f3a9943ceccc7d8c9c0f6e48
lychees <lychees67@gmail.com>
2020-11-05 19:00:41 +0800

    Update fortube.sol

--- Contracts/Ethereum/contracts/stragegies/fortube.sol
+++ Contracts/Ethereum/contracts/stragegies/fortube.sol
@@ -14,18 +14,8 @@
 
 pragma solidity =0.6.12;
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function decimals() external view returns (uint);
-    function name() external view returns (string memory);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+import '../interfaces/IERC20.sol';
+import '../interfaces/IUnisaveV2Factory.sol';
 
 library SafeMath {
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -124,52 +114,14 @@ library SafeERC20 {
     }
 }
 
-interface Controller {
-    function vaults(address) external view returns (address);
-    function rewards() external view returns (address);
-}
 
-/*
- A strategy must implement the following calls;
- 
- - deposit()
- - withdraw(address) must exclude any tokens used in the yield - Controller role - withdraw should return to Controller
- - withdraw(uint) - Controller | Vault role - withdraw should always return to vault
- - withdrawAll() - Controller | Vault role - withdraw should always return to vault
- - balanceOf()
- 
- Where possible, strategies must remain as immutable as possible, instead of updating variables, we update the contract by linking it in the controller
- 
-*/
-
-
-
-interface UniswapRouter {
-    function swapExactTokensForTokens(uint, uint, address[] calldata, address, uint) external;
-}
 interface For{
     function deposit(address token, uint256 amount) external payable;
     function withdraw(address underlying, uint256 withdrawTokens) external;
     function withdrawUnderlying(address underlying, uint256 amount) external;
     function controller() view external returns(address);
-
-}
-interface IFToken {
-    function balanceOf(address account) external view returns (uint256);
-
-    function calcBalanceOfUnderlying(address owner)
-        external
-        view
-        returns (uint256);
 }
 
-interface IBankController {
-
-    function getFTokeAddress(address underlying)
-        external
-        view
-        returns (address);
-}
 interface ForReward{
     function claimReward() external;
 }
@@ -181,134 +133,60 @@ contract StrategyFortube {
     
     address constant public output = address(0x658A109C5900BC6d2357c87549B651670E5b0539); //FOR
     address constant public fortube = address(0x0cEA0832e9cdBb5D476040D58Ea07ecfbeBB7672);//主合约.
-    address  public fortube_reward = address(0x55838F18e79cFd3EA22Eea08Bd3Ec18d67f314ed); //领取奖励的合约
-    
-    address  public want; 
-
-    
-    uint public strategyfee = 100;
-    uint public fee = 300;
-    uint public burnfee = 500;
-    uint public callfee = 100;
-    uint constant public max = 1000;
-
-    uint public withdrawalFee = 0;
-    uint constant public withdrawalMax = 10000;
-    
-    address public governance;
-    address public strategyDev;
-    address public controller;
-    address public burnAddress = 0xB6af2DabCEBC7d30E440714A33E5BD45CEEd103a;
-
-    string public getName;
-
-    address[] public swap2YFIIRouting;
-    address[] public swap2TokenRouting;
-    
-    
-    constructor(address _want) public {
-        want = _want;
-        governance = msg.sender;
-        controller = 0x5B916D02A9745C64EC6C0AFe41Ee4893Dd5a01B7;
-        getName = string(
-            abi.encodePacked("yfii:Strategy:", 
-                abi.encodePacked(IERC20(want).name(),"The Force Token"
-                )
-            ));
-        swap2YFIIRouting = [output,weth,yfii];
-        swap2TokenRouting = [output,weth,want];
-        doApprove();
-        strategyDev = tx.origin;
+    address public fortube_reward = address(0x55838F18e79cFd3EA22Eea08Bd3Ec18d67f314ed); //领取奖励的合约
+    address public factory;
+        
+    modifier onlyOwner() {
+        require(owner() == msg.sender, "Ownable: caller is not the owner");
+        _;
     }
 
+    function owner() public view returns (address) {
+        return IUnisaveV2Factory(factory).feeTo();
+    }    
+    
     function setFortubeReward(address _reward) public {
         require(msg.sender == governance, "!governance");
         fortube_reward = _reward;
     }
     
     
-    function deposit() public {
+    // deposit
+    function deposit(uint a) public {
         uint _want = IERC20(want).balanceOf(address(this));
         address _controller = For(fortube).controller();
         if (_want > 0) {
             IERC20(want).safeApprove(_controller, 0);
             IERC20(want).safeApprove(_controller, _want);
             For(fortube).deposit(want,_want);
-        }        
+        }
     }
-    
-    // Controller only function for creating additional rewards from dust
-    function withdraw(IERC20 _asset) external returns (uint balance) {
-        require(msg.sender == controller, "!controller");
-        require(want != address(_asset), "want");
-        balance = _asset.balanceOf(address(this));
-        _asset.safeTransfer(controller, balance);
-    }
-    
+        
     // Withdraw partial funds, normally used with a vault withdrawal
-    function withdraw(uint _amount) external {
+    function withdraw(address want, uint _amount) external { // TODO 记账
         require(msg.sender == controller, "!controller");
         uint _balance = IERC20(want).balanceOf(address(this));
         if (_balance < _amount) {
             _amount = _withdrawSome(_amount.sub(_balance));
             _amount = _amount.add(_balance);
         }
-        
-        uint _fee = 0;
-        if (withdrawalFee>0){
-            _fee = _amount.mul(withdrawalFee).div(withdrawalMax);        
-            IERC20(want).safeTransfer(Controller(controller).rewards(), _fee);
-        }
-        
-        
+                        
         address _vault = Controller(controller).vaults(address(want));
         require(_vault != address(0), "!vault"); // additional protection so we don't burn the funds
-        IERC20(want).safeTransfer(_vault, _amount.sub(_fee));
+        IERC20(want).safeTransfer(_vault, _amount);
     }
-    
-    // Withdraw all funds, normally used when migrating strategies
-    function withdrawAll() external returns (uint balance) {
-        require(msg.sender == controller || msg.sender == governance,"!governance");
-        _withdrawAll();
-        
-        
-        balance = IERC20(want).balanceOf(address(this));
-        
-        address _vault = Controller(controller).vaults(address(want));
-        require(_vault != address(0), "!vault"); // additional protection so we don't burn the funds
-        IERC20(want).safeTransfer(_vault, balance);
-    }
-    
-    function _withdrawAll() internal {
-        address _controller = For(fortube).controller();
-        IFToken fToken = IFToken(IBankController(_controller).getFTokeAddress(want));
-        uint b = fToken.balanceOf(address(this));
-        For(fortube).withdraw(want,b);
-    }
-    
-    function harvest() public {
-        require(!Address.isContract(msg.sender),"!contract");
+
+    function claim() public {
         ForReward(fortube_reward).claimReward();
+    }
+    
+    function harvest() public {        
         IERC20(output).transfer(owner());
     }
 
-    function _withdrawSome(uint256 _amount) internal returns (uint) {
+    function _withdrawSome(address want, uint256 _amount) internal returns (uint) {
         For(fortube).withdrawUnderlying(want,_amount);
         return _amount;
-    }
-    
-    function balanceOfWant() public view returns (uint) {
-        return IERC20(want).balanceOf(address(this));
-    }
-    
-    function balanceOfPool() public view returns (uint) {
-        address _controller = For(fortube).controller();
-        IFToken fToken = IFToken(IBankController(_controller).getFTokeAddress(want));
-        return fToken.calcBalanceOfUnderlying(address(this));
-    }
-        
-    function balanceOf() public view returns (uint) {
-        return balanceOfWant()
-               .add(balanceOfPool());
+    }d(balanceOfPool());
     }
 }
