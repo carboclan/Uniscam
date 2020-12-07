f66a036c35fd076fc759186e9ab08aeb98a1dbd4
Garfield Lee <openpgp@550.moe>
2020-12-07 12:13:53 +0800

    feat: add safeApprove to pair contract

--- Contracts/Ethereum/contracts/UnisaveV2Pair.sol
+++ Contracts/Ethereum/contracts/UnisaveV2Pair.sol
@@ -7,10 +7,12 @@ import './interfaces/IERC20.sol';
 import './interfaces/IyToken.sol';
 import './interfaces/IUnisaveV2Factory.sol';
 import './interfaces/IUnisaveV2Callee.sol';
+import './utils/Address.sol';
 
 contract UnisaveV2Pair is UnisaveV2ERC20 {
     using SafeMathUnisave for uint;
     using UQ112x112 for uint224;
+    using Address for address;
 
     uint public constant MINIMUM_LIQUIDITY = 10**3;
     bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
@@ -258,6 +260,42 @@ contract UnisaveV2Pair is UnisaveV2ERC20 {
         fee = _fee;
 
         emit FeeUpdated(_fee);
+    }
+
+    // openzeppelin SafeERC20
+    /**
+     * @dev Deprecated. This function has issues similar to the ones found in
+     * {IERC20-approve}, and its usage is discouraged.
+     *
+     * Whenever possible, use {safeIncreaseAllowance} and
+     * {safeDecreaseAllowance} instead.
+     */
+    function safeApprove(IERC20 token, address spender, uint256 value) internal {
+        // safeApprove should only be called when setting an initial allowance,
+        // or when resetting it to zero. To increase and decrease it, use
+        // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
+        // solhint-disable-next-line max-line-length
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+    /**
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must not be false).
+     * @param token The token targeted by the call.
+     * @param data The call data (encoded using abi.encode or one of its variants).
+     */
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+        // we're implementing it ourselves. We use {Address.functionCall} to perform this call, which verifies that
+        // the target address contains contract code and also asserts for success in the low-level call.
+
+        bytes memory returndata = address(token).functionCall(data, "SafeERC20: low-level call failed");
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
     }
 
     // vault
